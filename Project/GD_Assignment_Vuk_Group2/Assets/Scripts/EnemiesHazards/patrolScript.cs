aad2388443ceb6cdd25b0a48cc409ade9764c197
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-09-06 18:20:51 +0200

    First Prototype

--- Project/GD_Assignment_Vuk_Group2/Assets/Scripts/EnemiesHazards/patrolScript.cs
+++ Project/GD_Assignment_Vuk_Group2/Assets/Scripts/EnemiesHazards/patrolScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class patrolScript : MonoBehaviour
 {
     public float speed;
@@ -33,6 +33,14 @@ public class patrolScript : MonoBehaviour
             {
                 waitTime -= Time.deltaTime;
             }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
---
+++ Project/GD_Assignment_Vuk_Group2/Assets/Scripts/Rotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rotation : MonoBehaviour
+{
+    private float rotate2;
+    public float rotationSpeed;
+    public bool clockwise;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clockwise == false)
+        {
+            rotate2 += Time.deltaTime * rotationSpeed;
+
+        }
+        else 
+        {
+            rotate2 += -Time.deltaTime * rotationSpeed;
+        
+        }
+        transform.rotation = Quaternion.Euler(0, 0, rotate2);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Boua aint no way boua");
+            Application.Quit();
+        }
+    }
+}
