cc682b26f980c176c6938be01104606865ee7cb9
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-09-06 14:16:50 +0200

    help

---
+++ Project/GD_Assignment_Vuk_Group2/Assets/Scripts/EnemiesHazards/patrolScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrolScript : MonoBehaviour
+{
+    public float speed;
+    public Transform[] mSpots;
+    private int randomPos; // picks random positiion from  the mSpot arrey 
+
+    //Enemy Reaches Point 
+    private float waitTime;
+    public float startWaitTime;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomPos = Random.Range(0,mSpots.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, mSpots[randomPos].position, speed * Time.deltaTime); // moves object from current position to random mSpot. 
+
+        if (Vector2.Distance(transform.position,mSpots[randomPos].position) < 0.2f ) 
+        {
+            if (waitTime <= 0)
+            {
+                randomPos = Random.Range(0, mSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else 
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
