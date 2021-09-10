3dcedd5838667211fcb142eec67bac382b052fb0
2057172 <80460572+2057172@users.noreply.github.com>
2021-09-10 16:12:12 +0200

    Revert "Mask"
    This reverts commit 93806343c806091df615f4fc4401cb216647d014.

--- Project/GD_Assignment_Vuk_Group2/Assets/Scripts/EnemiesHazards/trapFollow.cs
+++ Project/GD_Assignment_Vuk_Group2/Assets/Scripts/EnemiesHazards/trapFollow.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class trapFollow : MonoBehaviour
 {
     public Transform player;
-    public float moveSpeed;
-    public LumosOnAndOff lumos;
+    public float moveSpeed; 
 
     private Rigidbody2D rBody;
 
@@ -14,7 +13,6 @@ public class trapFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
         rBody = this.GetComponent<Rigidbody2D>();
     }
 
@@ -28,22 +26,13 @@ public class trapFollow : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.transform.CompareTag("Player"))
-        if(collision.gameObject.tag == "MainLight" && lumos.LightOn == false)
+        if (collision.transform.CompareTag("Player"))
         {
             Vector3 direction = player.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             rBody.rotation = angle;
             direction.Normalize();
             movement = direction;
-
-        } else if(collision.gameObject.tag == "MainLight" && lumos.LightOn == true)
-        {
-            Vector3 direction = player.position - transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rBody.rotation = angle;
-            direction.Normalize();
-            movement = direction * 2;
         }
     }
 
